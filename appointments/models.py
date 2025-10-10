f80139b2af370683f3f5edb07d7802e6711e4dbc
alejo <alejandrocf2835@gmail.com>
2025-10-09 21:50:05 -0500

    fix(models): rename 'Appointmens' to 'Appointment' in Appointment model

--- appointments/models.py
+++ appointments/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from barbers.models import Barber
 from customers.models import Customer
 
-class Appointments(models.Model):
+class Appointment(models.Model):
   barber = models.ForeignKey(Barber, on_delete=models.CASCADE)
   customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True)
