98952853c1ae087a66eb8de5496bd504b45c9d4d
alejo <alejandrocardenas.dev@gmail.com>
2025-10-20 15:17:25 -0500

    feat(views, url): add endpoint to get barber's available times for a specific date with proper validations and URL registration

--- barbers/urls.py
+++ barbers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from barbers.views import EditBarberSchedule, GetBarber
+from barbers.views import EditBarberSchedule, GetBarber, GetBarberAvailableTimesSpecificDate
 
 urlpatterns = [
   path('get-barbers/', GetBarber.as_view()),
-  path('edit-barber-schedule/', EditBarberSchedule.as_view())
+  path('edit-barber-schedule/', EditBarberSchedule.as_view()),
+  path('get-barber/<int:barber_id>/available-times/', GetBarberAvailableTimesSpecificDate.as_view()),
 ]
--- barbers/views.py
+++ barbers/views.py
@@ -1,14 +1,57 @@
 from rest_framework import generics
+from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
+from appointments.models import Appointment
 from barbers.models import Barber
 from barbers.serializers import BarberSerializer, EditBarberScheduleSerializer
 from permissions import IsBarber, IsCustomer
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from datetime import datetime
+from rest_framework.permissions import AllowAny
+
 
 # Get Barbers
 class GetBarber(generics.ListAPIView):
   queryset = Barber.objects.all()
   serializer_class = BarberSerializer
   permission_classes = [IsCustomer]
+
+
+# Get Barbers' Working Hours
+class GetBarberAvailableTimesSpecificDate(APIView):
+  permission_classes = [IsCustomer]
+
+  def get(self, request, barber_id):
+    barber = get_object_or_404(Barber, id=barber_id)
+
+    date_str = request.query_params.get('date')
+
+    if not date_str:
+      return Response({'error': 'Missing date parameter. Example: ?date=2025-10-20'}, status=400)
+
+    try:
+      selected_date = datetime.strptime(date_str, '%Y-%m-%d').date()
+    except ValueError:
+      return Response({'error': 'Invalid date format. Use YYYY-MM-DD.'}, status=400)
+
+    available_times = barber.get_available_times()
+
+    appointments = Appointment.objects.filter(barber=barber, appointment_date=selected_date)
+    booked_appointments = appointments.values_list('appointment_start_time', flat=True)
+
+    available_times_for_date = []
+
+    for times in available_times:
+      if times not in booked_appointments:
+        available_times_for_date.append(times)
+
+    return Response({
+      'barber': barber.user.first_name,
+      'date': date_str,
+      'available_times': available_times_for_date
+    })
+
 
 # Edit Barber Schedule For Working
 class EditBarberSchedule(generics.UpdateAPIView):
