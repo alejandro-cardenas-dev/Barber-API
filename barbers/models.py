92f8e2fb86909ca4cb9787c9a6a6d93a2f6ae883
alejo <alejandrocardenas.dev@gmail.com>
2025-10-24 16:06:33 -0500

    feat(models): add lunch start and finish fields with their validations, change in get_available_times with lunch fields implementation, add validate_schedule_values to have global validations with method clean, save, full_clean and use of decorator @staticmethod in validate_schedule_values

--- barbers/models.py
+++ barbers/models.py
@@ -5,22 +5,59 @@ from django.core.exceptions import ValidationError
 
 class Barber(models.Model):
   user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-  time_to_start_working = models.TimeField(default=time(10, 0), blank=False, null=False)
-  time_to_finish_working = models.TimeField(default=time(16, 0), blank=False, null=False)
+  work_start_time = models.TimeField(default=time(10, 0), blank=False, null=False)
+  work_end_time = models.TimeField(default=time(16, 0), blank=False, null=False)
+  lunch_start_time = models.TimeField(default=time(12, 0))
+  lunch_end_time = models.TimeField(default=time(14, 0))
+
+  @staticmethod
+  def validate_schedule_values(work_start_time, work_end_time, lunch_start_time, lunch_end_time):
+    if work_start_time >= work_end_time:
+      raise ValidationError('Work start time must be earlier than finish time.')
+
+    if lunch_start_time >= lunch_end_time:
+      raise ValidationError('Lunch start time must be earlier than finish time.')
+
+    if not (work_start_time < lunch_start_time < work_end_time):
+      raise ValidationError({'lunch_start_time': 'Lunch start time must be during working hours.'})
+
+    if not (work_start_time < lunch_end_time < work_end_time):
+      raise ValidationError({'lunch_end_time': 'Lunch end time must be during working hours.'})
 
   def clean(self):
-    if self.time_to_start_working >= self.time_to_finish_working:
-      raise ValidationError('Start time must be earlier than finish time.')
-    return super().clean()
+    self.validate_schedule_values(
+      self.work_start_time,
+      self.work_end_time,
+      self.lunch_start_time,
+      self.lunch_end_time
+    )
+    super().clean()
+
+  def save(self, *args, **kwargs):
+    self.full_clean()
+    super().save(*args, **kwargs)
+
 
   # Feat: change name get_available_times -> get_barber_work_times
   def get_available_times(self, interval=30):
     times = []
+    rest_times = []
 
-    current = datetime.combine(datetime.today(), self.time_to_start_working)
-    end = datetime.combine(datetime.today(), self.time_to_finish_working)
+    current = datetime.combine(datetime.today(), self.work_start_time)
+    end = datetime.combine(datetime.today(), self.work_end_time)
+    lunch_start = datetime.combine(datetime.today(), self.lunch_start_time)
+    lunch_end = datetime.combine(datetime.today(), self.lunch_end_time)
+
+
+    while lunch_start < lunch_end:
+      rest_times.append(lunch_start)
+      lunch_start += timedelta(minutes=interval)
 
     while current < end:
+      if current in rest_times:
+        current += timedelta(minutes=interval)
+        continue
+
       times.append(current.strftime('%H:%M'))
       current += timedelta(minutes=interval)
 
