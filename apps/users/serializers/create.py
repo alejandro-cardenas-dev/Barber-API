630db96dd4298dad633814d085a77c770a4891dd
alejo <alejandrocardenas.dev@gmail.com>
2026-02-26 11:16:55 -0500

    fix(serializers): improve validation and error messages in CreateUserSerializer
    - Fix field-level validators returning nested error dicts
    - Add regex validation to phone field
    - Replace manual password length check with Django's built-in validate_password
    - Add explicit required=True to password and password2 fields
    - Organize imports following PEP 8 conventions

--- apps/users/serializers/create.py
+++ apps/users/serializers/create.py
@@ -1,11 +1,17 @@
+import re
+
+from django.contrib.auth.password_validation import validate_password as django_validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
+
 from rest_framework import serializers
+
 from apps.users.models import User
 from apps.users.services.user_registration import register_user
 
 
 class CreateUserSerializer(serializers.ModelSerializer):
-  password = serializers.CharField(write_only=True)
-  password2 = serializers.CharField(write_only=True)
+  password = serializers.CharField(write_only=True, required=True)
+  password2 = serializers.CharField(write_only=True, required=True)
 
   class Meta:
     model = User
@@ -13,6 +19,28 @@ class CreateUserSerializer(serializers.ModelSerializer):
       'id', 'first_name', 'last_name', 'email', 'phone',
       'password', 'password2', 'is_barber', 'is_customer'
     ]
+
+  def validate_password(self, value):
+    try:
+      django_validate_password(value)
+    except DjangoValidationError as e:
+      raise serializers.ValidationError(e.messages)
+    return value
+
+  def validate_phone(self, value):
+    if not re.fullmatch(r'\d{10}', value):
+        raise serializers.ValidationError("Phone must contain exactly 10 digits.")
+    return value
+
+  def validate_first_name(self, value):
+    if len(value) < 3:
+      raise serializers.ValidationError("Name must have 3 or more characters")
+    return value
+
+  def validate_last_name(self, value):
+    if len(value) < 3:
+      raise serializers.ValidationError("Last name must have 3 or more characters"  )
+    return value
 
   def validate(self, attrs):
     attrs = super().validate(attrs)
@@ -27,35 +55,6 @@ class CreateUserSerializer(serializers.ModelSerializer):
       )
 
     return attrs
-
-  def validate_password(self, value):
-    if len(value) < 8:
-      raise serializers.ValidationError(
-        {"password": "Password must contain 8 or more characters"}
-      )
-    return value
-
-  def validate_phone(self, value):
-    if len(value) != 10:
-      raise serializers.ValidationError(
-        {"phone": "Phone must contain exactly 10 digits"}
-      )
-    return value
-
-  def validate_first_name(self, value):
-    if len(value) < 3:
-      raise serializers.ValidationError(
-        {"first_name": "Name must have 3 or more characters"}
-      )
-    return value
-
-  def validate_last_name(self, value):
-    if len(value) < 3:
-      raise serializers.ValidationError(
-        {"last_name": "Last name must have 3 or more characters"}
-      )
-    return value
-
 
   def create(self, validated_data):
     password = validated_data.pop('password')
